a344d3909aa050871946445d99c886be6db16b7c
Marcin Bedus <47300162+marcinbedus@users.noreply.github.com>
2022-02-08 14:45:16 +0100

    prisma fix

--- client/src/components/shared/RequestForm/logic.ts
+++ client/src/components/shared/RequestForm/logic.ts
@@ -4,18 +4,6 @@ export const useRequestFormLogic = () => {
   const baseValidationSchema = Yup.object().shape({
     url: Yup.string().url("Must be valid url").required("Required"),
     method: Yup.string().required("Required"),
-    headers: Yup.array(
-      Yup.object({
-        key: Yup.string().required("Required"),
-        value: Yup.string().required("Required"),
-      })
-    ),
-    // body: Yup.array(
-    //   Yup.object({
-    //     key: Yup.string().required("Required"),
-    //     value: Yup.string().required("Required"),
-    //   })
-    // ),
   });
 
   return { baseValidationSchema };
